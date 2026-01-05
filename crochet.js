6fbc8dee9d852249c7678075ae80be133034c1ba
Akansha <akankshaj2005@gmail.com>
2026-01-05 16:00:51 +0530

    Add files via upload

---
+++ crochet.js
@@ -0,0 +1,43 @@
+// PRODUCT DATA
+const products = [
+  { name: "Crochet Bag", category: "bag", image: "images/bag.jpeg", price: "₹399" },
+  { name: "Handmade Tote", category: "bag", image: "images/bag2.jpg", price: "₹70" },
+  { name: "Crochet Top", category: "top", image: "images/top1.jpg", price: "₹70" },
+  { name: "Evileye Keychain", category: "keychain", image: "images/evileye.jpeg", price: "₹80" },
+  { name: "Sunflower Keychain", category: "keychain", image: "images/sunflower.jpeg", price: "₹70" },
+  { name: "Cherry Keychain", category: "keychain", image: "images/cherry.jpeg", price: "₹70" },
+  { name: "Lily Flower", category: "flower", image: "images/lily.jpeg", price: "₹90" }
+];
+
+const productList = document.getElementById("productList");
+
+// SHOW PRODUCTS
+function displayProducts(items) {
+  productList.innerHTML = "";
+
+  items.forEach(product => {
+    const div = document.createElement("div");
+    div.className = "product-card";
+
+    div.innerHTML = `
+      <img src="${product.image}">
+      <h3>${product.name}</h3>
+      <p class="price">${product.price}</p>
+    `;
+
+    productList.appendChild(div);
+  });
+}
+
+// FILTER PRODUCTS
+function filterProducts(category) {
+  if (category === "all") {
+    displayProducts(products);
+  } else {
+    const filtered = products.filter(p => p.category === category);
+    displayProducts(filtered);
+  }
+}
+
+// INITIAL LOAD
+displayProducts(products);
