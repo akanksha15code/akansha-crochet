cb26b1db173884c49d214b165f0ed78588e8f087
Akansha <akankshaj2005@gmail.com>
2026-01-05 17:19:46 +0530

    Update crochet.js

--- crochet.js
+++ crochet.js
@@ -1,12 +1,12 @@
 // PRODUCT DATA
 const products = [
-  { name: "Crochet Bag", category: "bag", image: "images/bag.jpeg", price: "₹399" },
-  { name: "Handmade Tote", category: "bag", image: "images/bag2.jpg", price: "₹70" },
-  { name: "Crochet Top", category: "top", image: "images/top1.jpg", price: "₹70" },
-  { name: "Evileye Keychain", category: "keychain", image: "images/evileye.jpeg", price: "₹80" },
-  { name: "Sunflower Keychain", category: "keychain", image: "images/sunflower.jpeg", price: "₹70" },
-  { name: "Cherry Keychain", category: "keychain", image: "images/cherry.jpeg", price: "₹70" },
-  { name: "Lily Flower", category: "flower", image: "images/lily.jpeg", price: "₹90" }
+  { name: "Crochet Bag", category: "bag", image: "bag.jpeg", price: "₹399" },
+  { name: "Handmade Tote", category: "bag", image: "bag2.jpg", price: "₹70" },
+  { name: "Crochet Top", category: "top", image: "top1.jpg", price: "₹70" },
+  { name: "Evileye Keychain", category: "keychain", image: "evileye.jpeg", price: "₹80" },
+  { name: "Sunflower Keychain", category: "keychain", image: "sunflower.jpeg", price: "₹70" },
+  { name: "Cherry Keychain", category: "keychain", image: "cherry.jpeg", price: "₹70" },
+  { name: "Lily Flower", category: "flower", image: "lily.jpeg", price: "₹90" }
 ];
 
 const productList = document.getElementById("productList");
@@ -41,3 +41,4 @@ function filterProducts(category) {
 
 // INITIAL LOAD
 displayProducts(products);
+
